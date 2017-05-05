64ed3405442b0b310c6a778e29806ce5783d95d2
zjy <zjy@QE016>
2017-05-05 09:28:55 +0800

    4444

--- src/test/FirstServlet.java
+++ src/test/FirstServlet.java
@@ -27,7 +27,7 @@ public class FirstServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: 的点点滴滴2222").append(request.getContextPath());
+		response.getWriter().append("Served at: 的点点滴滴222244444").append(request.getContextPath());
 	}
 
 	/**
